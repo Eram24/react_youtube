69cda25bf9dc5e3806f520658f2e459050b9de6e
Eram24 <eramfaiz24@gmail.com>
2025-04-11 10:50:50 +0530

    compenent composition

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,24 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
 
-// function App() {
-//   const [count, setCount] = useState(0)
+ export const Card=()=> {(<div>
+    <Series/>
+    <Series></Series>
+     {Series}
+     </div>)
+ };
 
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vite.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-
-// export default App
-
- const App=()=>{
-  return <h1>Hello, Eram </h1>
+const Series=()=>{
+  return (
+    <div>
+      
+        <img src="/hen.jpg" alt="" width="400px" />
+      
+      <h1>Name: Series name</h1>
+      <h2>Rating : 8.5</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur consectetur nisi ullam adipisci recusandae unde placeat veritatis sunt quidem, minus est eaque quas nostrum rerum eligendi aliquid assumenda iusto porro.</p>
+    </div>
+  )
 }
-export default App
-
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import {Card} from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Card />
   </StrictMode>,
 )
