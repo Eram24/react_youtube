e75a24a1232b742375304fbc4b5d558ba89974db
Eram24 <eramfaiz24@gmail.com>
2025-04-11 20:25:40 +0530

    loop through map

--- src/components/Series.jsx
+++ src/components/Series.jsx
@@ -1,13 +1,49 @@
-export const Header=()=>{
-    return <h1>Header</h1>;
-}
+// export const Header=()=>{
+//     return <h1>Header</h1>;
+// }
 
-export const Footer=()=>{
-    return <h1>Footer</h1>;
-}
+// export const Footer=()=>{
+//     return <h1>Footer</h1>;
+// }
 
-const Card=()=>{
-    return <h1>This is default export</h1>
-}
+// const Card=()=>{
+//     return <h1>This is default export</h1>
+// }
 
-export default Card;
+// export default Card;
+
+import seriesData from "../api/seriesData.json"
+
+export const Series=()=>{
+    return (
+    <ul>
+       {
+        seriesData.map((currVal)=>{
+            return (
+        <li key={currVal.id}>
+             
+            <div>
+                <img src={currVal.img_url}  alt=""  width="40%" height="40%"/>
+            </div>
+            <h2>Name : {currVal.name}</h2>
+            <h3>Rating : {currVal.rating}</h3>
+            <p>Summary : {currVal.description}</p>
+            <p>Genre : {currVal.genre}</p>
+            <p>Cast : {currVal.cast}</p>
+            <a href={currVal.watch_url} target="_blank">
+            <button>Watch Now</button>
+            </a>
+            </li>
+          )
+          
+        })
+       }
+
+        
+        
+    
+        
+    </ul>
+      
+    );
+};      
--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 // import {Card} from './App.jsx'
 // import {Practice} from './Practice.jsx'
-import Card,{Header,Footer} from "./components/Series";
+// import Card,{Header,Footer} from "./components/Series";
+
+import {Series} from "./components/Series"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   <Header/>
-   <Card/>
-   <Card/>
-   <Card/>
-   <Card/>
-   <Footer/>
+   <Series></Series>
   </StrictMode>,
 )
