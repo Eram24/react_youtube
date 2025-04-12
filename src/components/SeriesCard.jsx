03918039eb804c9c05af1e6d6682c2cec04daae5
Eram24 <eramfaiz24@gmail.com>
2025-04-12 10:29:22 +0530

    further destructuring

--- src/components/SeriesCard.jsx
+++ src/components/SeriesCard.jsx
@@ -1,18 +1,18 @@
 export const SeriesCard=(props)=>{
     // console.log(props);
-    const { data } = props;
-    console.log(data);
+    const { img_url,name,rating,description,genre,cast,watch_url } = props.data;
+    console.log(name);
     return (
         <li >
             <div>
-                <img src={data.img_url}  alt=""  width="40%" height="40%"/>
+                <img src={img_url}  alt=""  width="40%" height="40%"/>
             </div>
-            <h2>Name : {data.name}</h2>
-            <h3>Rating : {data.rating}</h3>
-            <p>Summary : {data.description}</p>
-            <p>Genre : {data.genre}</p>
-            <p>Cast : {data.cast}</p>
-            <a href={data.watch_url} target="_blank">
+            <h2>Name : {name}</h2>
+            <h3>Rating : {rating}</h3>
+            <p>Summary : {description}</p>
+            <p>Genre : {genre}</p>
+            <p>Cast : {cast}</p>
+            <a href={watch_url} target="_blank">
             <button>Watch Now</button>
             </a>
             </li>
